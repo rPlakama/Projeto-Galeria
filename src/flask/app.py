f8ed8a7e2d1015b73503735ea2b946ebb35a13cc
rPlakama <rPlakama@proton.me>
2025-11-11 12:46:18 -0300

    Teste de conexão com a DB, artitas (funcionado) http://127.0.0.1:5000/artistas

--- src/flask/app.py
+++ src/flask/app.py
@@ -3,11 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mariadb+mariadbconnector://rPlakama:jaquksww@localhost/Galeria"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mariadb+mariadbconnector://rPlakama:jaquksww@localhost/Galeria"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index(name=None):
     return render_template("index.html", person=name)
+
+
+@app.route("/artistas")
+def artistas():
+    artistas = Artista.query.all()
+    return render_template("artistas.html", artistas=artistas)
+
+
+class Artista(db.Model):
+    __tablename__ = "artistas"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(255), nullable=False)
+    periodo_atuacao = db.Column(db.String(255))
+    nacionalidade = db.Column(db.String(255))
